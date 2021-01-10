21546145b611a896e1eec2dacfcf989311c9fef9
OoMiDOoO <midomaxgomaa@gmail.com>
2021-01-10 13:59:13 +0200

    Improved stylng, and solved an issue

--- script.py
+++ script.py
@@ -5,18 +5,33 @@
 import csv
 from openpyxl import Workbook
 from openpyxl import load_workbook
+from openpyxl.styles import Font, Alignment
 
-with open("output.csv") as output:
+with open("output.csv") as output_table:
     wb = load_workbook(filename = 'table.xlsx')
     ws = wb.active
     ws.title = "table"
 
-    output = csv.reader(output, delimiter=',')
-    for row in output:
+    output_table = csv.reader(output_table, delimiter=',')
+
+    # Find another way to acess last row.
+    row = output_table
+    for row in output_table:
+        # Similarly here, Find another way to deal with last row
+        if(len(row) == 1): 
+            break
+
         days = int(row[4])
         period_start = int(row[5][0])
         period_end = int(row[5][2])
-        for i in range(period_start, period_end + 1):
-            ws[f"{chr(i + 65)}{days + 2}"] = row[0] + row[1] + row[2] + row[3]
 
-    wb.save('test.xlsx')
+        #This should be changed to the preferred format by the module.
+        for i in range(period_start, period_end + 1):
+            ws[f"{chr(i + 65)}{days + 2}"].font = Font(size = 10)
+            ws[f"{chr(i + 65)}{days + 2}"] = f"{row[0]} \n- {row[1]} - G{row[3]} \n- Units: {row[2]}"
+            ws[f"{chr(i + 65)}{days + 2}"].alignment = Alignment(wrap_text = True, horizontal= 'center', vertical= 'center')
+            
+    ws['B9'] = row[-1][0]
+    ws['B9'].font = Font(size = 10)
+    ws['B9'].alignment = Alignment(wrap_text = True, horizontal= 'center', vertical= 'center')
+    wb.save('table.xlsx')
